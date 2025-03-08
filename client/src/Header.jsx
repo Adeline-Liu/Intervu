3da860e1e4d76e44624ea5fdedde4371a13c5e7d
Adeline-Liu <yifei05@outlook.com>
2025-03-08 13:32:20 -0800

    Add Header, Footer, and CoverPage components

---
+++ client/src/Header.jsx
@@ -0,0 +1,26 @@
+import { useNavigate } from "react-router-dom";
+
+const Header = () => {
+  const navigate = useNavigate();
+
+  const handleHomeClick = () => {
+    navigate("/");
+  };
+
+  return (
+    <nav className="text-3xl bg-darkBlue text-white sticky">
+      <div className="px-[100px] py-[20px] top-0 flex justify-between w-full items-center">
+        <div style={{ position: "relative" }}>
+          <button
+            className="fancyButton font-bold px-[32px] py-[20px]"
+            onClick={handleHomeClick}
+          >
+            Intervu
+          </button>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
